129b20298911ec6eb40bc8727be04090a7028c30
ricardoricarte <dev.ricarte@gmail.com>
2022-11-25 13:35:12 -0300

    TODO Tarefas

---
+++ src/main/java/br/com/todolist/model/Task.java
@@ -0,0 +1,46 @@
+package br.com.todolist.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Setter
+@Getter
+@ToString
+public class Task {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private LocalDateTime deadLine;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+}
